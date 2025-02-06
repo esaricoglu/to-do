96507999d2409bc8f41b63dc81c2e99cbcb8f433
Emincan Sarıçoğlu <esaricoglu@gmail.com>
2025-02-06 12:18:39 +0100

    Update UserRepository.java

--- src/main/java/com/esaricoglu/repository/UserRepository.java
+++ src/main/java/com/esaricoglu/repository/UserRepository.java
@@ -5,10 +5,12 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
 public interface UserRepository extends JpaRepository<User, Long> {
 
     //    @Query(value = "FROM User u WHERE u.username = :username")
-    User findByUsername(String username);
+    Optional<User> findByUsername(String username);
 
 }
