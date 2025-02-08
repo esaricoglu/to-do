8ca93914cff9e03fe3a97bbd1b089cf1f9ef23cc
Emincan Sarıçoğlu <esaricoglu@gmail.com>
2025-02-08 21:09:53 +0100

    Update UserControllerImpl.java

--- src/main/java/com/esaricoglu/controller/impl/UserControllerImpl.java
+++ src/main/java/com/esaricoglu/controller/impl/UserControllerImpl.java
@@ -7,6 +7,7 @@ import com.esaricoglu.dto.DtoUserIU;
 import com.esaricoglu.model.User;
 import com.esaricoglu.service.IUserService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.*;
 
 @RestController("/api/users")
@@ -17,19 +18,19 @@ public class UserControllerImpl implements IUserController {
 
     @Override
     @GetMapping
-    public RootEntity<DtoUser> getUserDetails(User currentUser) {
+    public RootEntity<DtoUser> getUserDetails(@AuthenticationPrincipal User currentUser) {
         return RootEntity.ok(userService.getUserDetails(currentUser));
     }
 
     @Override
     @PostMapping("/update")
-    public RootEntity<DtoUser> update(DtoUserIU dtoUserIU, User currentUser) {
+    public RootEntity<DtoUser> update(@RequestBody DtoUserIU dtoUserIU, @AuthenticationPrincipal User currentUser) {
         return RootEntity.ok(userService.update(dtoUserIU, currentUser));
     }
 
     @Override
     @DeleteMapping("/delete")
-    public RootEntity<String> delete(User currentUser) {
+    public RootEntity<String> delete(@AuthenticationPrincipal User currentUser) {
         return RootEntity.ok(userService.delete(currentUser));
     }
 }
