e0003a748d4fdf99e939b0716c572ca0c19b23b2
Emincan Sarıçoğlu <esaricoglu@gmail.com>
2025-02-08 21:33:30 +0100

    Update JwtAuthenticationFilter.java

--- src/main/java/com/esaricoglu/jwt/JwtAuthenticationFilter.java
+++ src/main/java/com/esaricoglu/jwt/JwtAuthenticationFilter.java
@@ -48,7 +48,7 @@ public class JwtAuthenticationFilter extends OncePerRequestFilter {
                 UserDetails userDetails = userDetailsService.loadUserByUsername(username);
                 if (userDetails != null && !jwtService.isTokenExpired(token)) {
                     UsernamePasswordAuthenticationToken authentication =
-                            new UsernamePasswordAuthenticationToken(username, null, userDetails.getAuthorities());
+                            new UsernamePasswordAuthenticationToken(userDetails, null, userDetails.getAuthorities());
                     authentication.setDetails(userDetails);
                     SecurityContextHolder.getContext().setAuthentication(authentication);
                 }
