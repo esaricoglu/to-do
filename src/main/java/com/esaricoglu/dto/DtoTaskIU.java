5f2a5a2f97353f197fd82903ee451e1eba60ebbc
Emincan Sarıçoğlu <esaricoglu@gmail.com>
2025-02-12 12:39:52 +0100

    Update DtoTaskIU.java

--- src/main/java/com/esaricoglu/dto/DtoTaskIU.java
+++ src/main/java/com/esaricoglu/dto/DtoTaskIU.java
@@ -7,4 +7,36 @@ public class DtoTaskIU {
     private String description;
 
     private boolean completed;
+
+    public DtoTaskIU() {}
+
+    public DtoTaskIU(String title, String description, boolean completed) {
+        this.title = title;
+        this.description = description;
+        this.completed = completed;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public boolean isCompleted() {
+        return completed;
+    }
+
+    public void setCompleted(boolean completed) {
+        this.completed = completed;
+    }
 }
